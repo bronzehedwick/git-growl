7524c04ca1d92775c6ececf04a7c2bafb65436f9
Chris DeLuca <bronzehedwick@gmail.com>
2012-10-18 00:09:06 -0400

    Wrapped code blocks in functions for easier organization

--- install.sh
+++ install.sh
@@ -7,19 +7,22 @@ GIT_GROWL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 UNAME=$( uname )
 
 # Determine operating system
-if [ "$UNAME" == 'Darwin' ]
-then
-    OS="osx"
-elif [ "$UNAME" == 'Linux' ]
-then
-    OS="linux"
-else
-    OS='unknown'
-    echo "You're on an OS that's too exotic for this script! Please install something else and try again."
-    exit 0
-fi
+platform ()
+{
+  if [ "$UNAME" == 'Darwin' ]
+  then
+      OS="osx"
+  elif [ "$UNAME" == 'Linux' ]
+  then
+      OS="linux"
+  else
+      echo "\033[0;31mError:\033[39m You're using an OS that's too exotic for this script! Please install something else and try again."
+      exit 0
+  fi
 
-FILES=$GIT_GROWL_DIR/$OS/*
+  FILES=$GIT_GROWL_DIR/$OS/*
+  find_git
+}
 
 # Install function
 setup ()
@@ -34,13 +37,18 @@ setup ()
 }
 
 # Check if we're in a git repo, then if we are determine the top level directory
-if [ $(git rev-parse --show-toplevel) != "fatal: Not a git repository (or any of the parent directories): .git" ]
-then
-    TARGET=$(git rev-parse --show-toplevel)
-    TARGET=$TARGET"/.git/hooks/"
-    setup
-else
-    echo "\033[0;31mError:\033[39m You're not in a git repository!\nPlease navigate to one and run this script again"
-    exit 1
-fi
+find_git ()
+{
+  if [ $(git rev-parse --show-toplevel) != "fatal: Not a git repository (or any of the parent directories): .git" ]
+  then
+      TARGET=$(git rev-parse --show-toplevel)
+      TARGET=$TARGET"/.git/hooks/"
+      setup
+  else
+      echo "\033[0;31mError:\033[39m You're not in a git repository!\nPlease navigate to one and run this script again"
+      exit 1
+  fi
+}
+
+platform
 
