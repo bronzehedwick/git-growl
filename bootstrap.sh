d86b15bacaf5b94e1dcacb159a27d924bf937fb0
Chris DeLuca <bronzehedwick@gmail.com>
2012-10-23 01:59:07 -0400

    Changed branch to checkout to something that exists
    When the script is closer to done, I'll make a real branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ echo "Thanks for installing git-growl\n"
 
 if [ ! -e $endpath/.git ]; then
     echo "cloning git-growl\n"
-    git clone --recursive -b alpha-1 http://github.com/bronzehedwick/git-growl.git $endpath
+    git clone --recursive -b master http://github.com/bronzehedwick/git-growl.git $endpath
 else
     echo "updating git-growl\n"
     cd $endpath && git pull
