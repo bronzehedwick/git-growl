3f7c28c23c5571170b34924d5057cd9e25797709
Chris DeLuca <bronzehedwick@gmail.com>
2012-10-23 02:02:28 -0400

    Added success message after install

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 endpath="$HOME/.git-growl"
 
 
-echo "Thanks for installing git-growl\n"
+echo "Thanks for installing git-growl!\n"
 
 
 if [ ! -e $endpath/.git ]; then
@@ -13,4 +13,6 @@ else
     echo "updating git-growl\n"
     cd $endpath && git pull
 fi
+
+echo "Success! Documentation can be found at $endpath/README.md"
 
