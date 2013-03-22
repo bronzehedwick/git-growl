4fd0111f6e5feb868ce3fb9535363349ee618738
Chris DeLuca <bronzehedwick@gmail.com>
2013-03-22 19:52:54 -0400

    Added notification system checks
    This is immediately useful for kde integration.

--- install.sh
+++ install.sh
@@ -17,7 +17,28 @@ platform ()
       OS="linux"
   else
       echo "\033[0;31mError:\033[39m You're using an OS that's too exotic for this script! Please install something else and try again."
-      exit 0
+      exit 1
+  fi
+
+  # Check what notification system is installed
+  if [ $OS == "linux" ] ; then
+    which libnotify > /dev/null
+    if [ $? != 0 ] ; then
+      NOTIF="libnotify"
+    fi
+    which kdialog > /dev/null
+    if [ $? == 0 ] ; then
+      OS=$OS/kde
+    else
+      echo "\033[0;31mError:\033[39m You don't have a supported notification system installed. Please install either libnotify or knotify4 to continue."
+    fi
+  elif [ $OS = "Darwin" ] ; then
+    which growlnotify > /dev/null
+    if [ $? == 0 ] ; then
+      NOTIF="growlnotify"
+    else
+      echo "\033[0;31mError:\033[39m growlnotify is not installed. Please install it from the following URL: http://growl.info/downloads#generaldownloads"
+    fi
   fi
 
   FILES=$GIT_GROWL_DIR/$OS/*
@@ -39,8 +60,7 @@ setup ()
 # Check if we're in a git repo, then if we are determine the top level directory
 find_git ()
 {
-  if [ $(git rev-parse --show-toplevel) != "fatal: Not a git repository (or any of the parent directories): .git" ]
-  then
+  if [ $(git rev-parse --show-toplevel) != "fatal: Not a git repository (or any of the parent directories): .git" ] ; then
       TARGET=$(git rev-parse --show-toplevel)
       TARGET=$TARGET"/.git/hooks/"
       setup
